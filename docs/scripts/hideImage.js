b48f3749f4afd3a4b1447e7d9466cf3c40678787
Eric <ericfaust@web.de>
2025-06-26 16:46:22 +0200

    added help

--- docs/scripts/hideImage.js
+++ docs/scripts/hideImage.js
@@ -1,12 +1,29 @@
 const btn   = document.getElementById('displayButton');
 const img = document.getElementById('hiddenImage');
 
-  btn.addEventListener('click', () => {
-    const isHidden = img.hidden;
-    img.hidden   = !isHidden;                    // invert
-    if (isHidden){
-		btn.textContent = 'Lösung verstecken'
-	}else{
-		btn.textContent = 'Anzeigen'
-	}
-  });
+
+
+btn.addEventListener('click', () => {
+  const isHidden = img.hidden;
+  img.hidden   = !isHidden;
+
+  if (isHidden) {
+	  btn.textContent = 'Lösung verstecken';
+  } else {
+	  btn.textContent = 'Anzeigen';
+  }
+});
+
+const btnList = document.getElementById('listButton');
+const list = document.getElementById('hiddenList');
+
+btnList.addEventListener('click', () => {
+  const isHidden = list.hidden;
+  list.hidden = !isHidden;
+
+  if (isHidden) {
+    btnList.textContent = 'Lösung verstecken';
+  } else {
+    btnList.textContent = 'Hilfe';
+  }
+});
