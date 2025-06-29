0a09b92b0bad5fe5421978bcd5bae8798bc2eff3
Eric <ericfaust@web.de>
2025-06-29 22:27:48 +0200

    include less boredom

---
+++ docs/scripts/hideText.js
@@ -0,0 +1,13 @@
+const gpuButton = document.getElementById('GPUTextButton');
+const gpuText = document.getElementById('hiddenGPUText');
+
+gpuButton.addEventListener('click', () => {
+  const isHidden = gpuText.hidden;
+  gpuText.hidden = !isHidden;
+
+  if (isHidden) {
+    gpuButton.textContent = 'Lösung verstecken';
+  } else {
+    gpuButton.textContent = 'Anzeigen';
+  }
+});
